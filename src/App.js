0de569897c2e1e415ef715e980a89c929d6ec51c
Oscar Coghlan <oscar.coghlan@gmail.com>
2023-10-02 22:51:39 +1100

    just made a generalised item component, dunno how useful it is

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import logo from './logo.svg';
 import About from "./components/About.js";
 import Landing from "./components/Landing.js";
 import Scripts from "./Scripts/Scripts.js";
+import Project from "./components/Project";
 
 function App() {
   return (
-    <div className="App bg-black text-gray-400 body-font">
+    <div className="App bg-black text-gray-200">
       <Helmet>
         <link href="/styles.css" rel="stylesheet" />
       </Helmet>
       <Landing />
-      <About />
+      <Project 
+         heading={"About me"}
+         content={`I'm Oscar, a dedicated software engineering student focused on delivering innovative solutions. On this portfolio website, I share my skills, projects, and experiences.\n
+              From an early age, I've been intrigued by the possibilities of software engineering. I find fulfillment in creating elegant and efficient solutions to complex challenges, combining creativity and problem-solving. As a student, I actively pursue opportunities for growth and contribution in this ever-evolving field.\n
+              I am currently completing a degree in software engineering, and have developed a strong foundation in programming languages, data structures, algorithms, and software design principles. I have extended many classes with Java, wrestled with the c++ standard library, and returned back to the basics with c. I've dipped my toes into .NET, and I've learnt how to express myself on the internet with PHP. I'm consistently curious about exploring new and old technologies.\n
+              Beyond academia, I thrive in collaborative environments, valuing effective communication and a positive impact. Approachable and enthusiastic, I appreciate diverse perspectives and eagerly contribute my own ideas. I am actively seeking meaningful opportunities to apply my skills, expand my expertise as a software engineer.\n`}
+         bgcolor={"bg-slate-900"}
+         align={'left'}
+      />
+      <Project 
+         heading={"This is heading"}
+         content={"This is content"}
+         bgcolor={"bg-slate-700"}
+         align={'left'}
+      />
       <Scripts />
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -2,23 +2,20 @@ function About() {
   return (
     <section id="about">
       <div className="bg-slate-900 w-full">
-        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center h-screen bg-slate-900">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left text-justify mb-16 md:mb-0 items-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white ">
               About me
             </h1>
             <p className="mb-8 leading-relaxed">
-              <br/>
+              <br />
               I'm Oscar, a dedicated software engineering student focused on delivering innovative solutions. On this portfolio website, I share my skills, projects, and experiences.
-
               <br />
               <br />
               From an early age, I've been intrigued by the possibilities of software engineering. I find fulfillment in creating elegant and efficient solutions to complex challenges, combining creativity and problem-solving. As a student, I actively pursue opportunities for growth and contribution in this ever-evolving field.
-
               <br />
               <br />
               I am currently completing a degree in software engineering, and have developed a strong foundation in programming languages, data structures, algorithms, and software design principles. I have extended many classes with Java, wrestled with the c++ standard library, and returned back to the basics with c. I've dipped my toes into .NET, and I've learnt how to express myself on the internet with PHP. I'm consistently curious about exploring new and old technologies.
-
               <br />
               <br />
               Beyond academia, I thrive in collaborative environments, valuing effective communication and a positive impact. Approachable and enthusiastic, I appreciate diverse perspectives and eagerly contribute my own ideas. I am actively seeking meaningful opportunities to apply my skills, expand my expertise as a software engineer.
